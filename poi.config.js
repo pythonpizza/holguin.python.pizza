da7d1ec3df759b1fd682dec43b628441d02b81c4
Lorenzo Peña <lorinkoz@gmail.com>
2020-07-28 15:22:18 -0400

    Multiple corrections

--- poi.config.js
+++ poi.config.js
@@ -1,22 +1,26 @@
 module.exports = {
   entry: "src/index",
   output: {
-    dir: "build/"
+    dir: "build/",
+    html: {
+      title: "Python Pizza Holguín",
+      gtag: "UA-173963719-1",
+    },
   },
   plugins: [
     {
       resolve: "@poi/plugin-typescript",
       options: {
-        vue: false
-      }
-    }
+        vue: false,
+      },
+    },
   ],
   configureWebpack(config, context) {
     config.resolve.extensions = [...config.resolve.extensions, ".tsx"];
     config.resolve.alias = {
       ...config.resolve.alias,
-      Assets: `${__dirname}/assets`
+      Assets: `${__dirname}/assets`,
     };
     return config;
-  }
+  },
 };
