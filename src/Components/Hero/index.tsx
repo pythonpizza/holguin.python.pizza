b3be6507072a363f4820083789483d702c143d0b
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-11 15:31:59 -0500

    fix: date in Home

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -27,7 +27,7 @@ export default class Hero extends React.Component {
               Cuba
             </a>
           </h2>
-          <h2>14 de noviembre de 2020</h2>
+          <h2>15 de enero de 2022</h2>
         </div>
 
         <Wave />
